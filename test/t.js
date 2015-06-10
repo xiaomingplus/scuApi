f177e54122d16dffa4033b0e4f14dced373c61e1
dsgygb <dsgygb@126.com>
2015-06-10 23:51:31 +0800

    test

--- test/t.js
+++ test/t.js
@@ -6,3 +6,26 @@ describe('Array', function(){
         })
     })
 })
+
+
+
+
+it('should return -1 when the value is not present', function(){
+    assert.equal(3, [1,2,3].indexOf(5));
+
+});
+
+
+
+
+it('should return -1 when the value is not present', function(){
+    assert.equal(3, [1,2,3].indexOf(5));
+
+});
+
+
+
+it('should return -1 when the value is not present', function(){
+    assert.equal(3, [1,2,3].indexOf(5));
+
+});
