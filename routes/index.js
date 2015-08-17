17253e9784f070c63ba36da49d09204776604b90
dsgygb <dsgygb@126.com>
2015-08-17 08:53:08 +0800

    for update

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ router.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By",' 3.2.1');
+  res.setHeader('content-type','application/json; charset=UTF-8');
   if(req.method=="OPTIONS") res.send(200);
   else  next();
 });
