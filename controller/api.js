b49b21fb1b42c0dd4f8daf3ff11bfb351b01e1a9
dsgygb <dsgygb@126.com>
2015-08-26 17:46:09 +0800

    for update

--- controller/api.js
+++ controller/api.js
@@ -365,8 +365,8 @@ api.examAgain = function(req,res){
                         list[i] = {
                             term: datas.termById[rr[i].termId].name,
                             examName:rr[i].examName,
-                            start:rr[i].start?(rr[i].start-8*60*60):0,
-                            end:rr[i].end?(rr[i].end-8*60*60):0,
+                            start:rr[i].start?(rr[i].start):0,
+                            end:rr[i].end?(rr[i].end):0,
                             name:rr[i].name,
                             campus:rr[i].campusId?datas.campusById[rr[i].campusId].name:"",
                             week:rr[i].week,
